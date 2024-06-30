2b1a3649ca4ee84d6466f38873059c40fe150c27
Nirmali Behera <136896725+NirmaliB11@users.noreply.github.com>
2024-06-30 22:35:13 +0530

    Time: 18 ms (43.53%), Space: 19.1 MB (12.32%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,47 +1,28 @@
 class Solution {
 public:
-    
-    bool topologicalSortCheck(unordered_map<int, vector<int>> &adj, int n, vector<int> &indegree) {
-        queue<int> que;
+    bool isCycleDFS(unordered_map<int, vector<int>> &adj, int u, vector<bool>& visited, vector<bool>& inRecursion) {
+        visited[u] = true;
+        inRecursion[u] = true;
         
-        int count = 0;
         
-        for(int i = 0; i<n; i++) {
-            if(indegree[i] == 0) {
-                count++;
-                que.push(i);
-            }
+        for(int &v : adj[u]) {
+            //if not visited, then we check for cycle in DFS
+            if(visited[v] == false && isCycleDFS(adj, v, visited, inRecursion))
+                return true;
+            else if(inRecursion[v] == true)
+                return true;
         }
         
+        inRecursion[u] = false;
+        return false;
         
-        while(!que.empty()) {
-            int u = que.front();
-            que.pop();
-            
-            for(int &v : adj[u]) {
-                
-                indegree[v]--;
-                
-                if(indegree[v] == 0) {
-                    count++;
-                    que.push(v);
-                }
-                
-            }
-            
-        }
-        
-        if(count == n) //I was able to visit all nodes (course)
-            return true; //i.e. I was able to finish all courses
-        
-        return false; //means there was a cycle and I couldn't complete all courses
     }
     
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         
         unordered_map<int, vector<int>> adj;
-        
-        vector<int> indegree(numCourses, 0); //kahn's algo
+        vector<bool> visited(numCourses, false);
+        vector<bool> inRecursion(numCourses, false);
         
         for(auto &vec : prerequisites) {
             int a = vec[0];
@@ -49,14 +30,13 @@ public:
             
             //b ---> a
             adj[b].push_back(a);
-            
-            //arrow ja raha hai 'a' me
-            indegree[a]++;
         }
         
+        for(int i = 0; i<numCourses; i++) {
+            if(!visited[i] && isCycleDFS(adj, i, visited, inRecursion))
+                return false;
+        }
         
-        //if cycle is present, not possible
-        
-        return topologicalSortCheck(adj, numCourses, indegree);
+        return true;
     }
 };
