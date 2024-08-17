6e57cadc492c101edc0836ddac8bc1decfafbf62
Nirmali Behera <136896725+NirmaliB11@users.noreply.github.com>
2024-08-17 16:19:37 +0530

    Time: 37 ms (5.93%), Space: 28.1 MB (8.25%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        if(nums[n-1]!=n) return n;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(i!=nums[i]) return i;
-        }
-        return 0;
+       unordered_set<int> st;
+       int n=nums.size();
+       for(auto &x:nums) st.insert(x);
+       for(int i=0;i<=n;i++){
+        if(st.find(i)==st.end()) return i;
+       }
+       return 0;
     }
 };
