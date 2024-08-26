057ecc8fcbbee6b5b60e0d007cd88e790613666e
Nirmali Behera <136896725+NirmaliB11@users.noreply.github.com>
2024-08-26 21:15:26 +0530

    Time: 14 ms (51.7%), Space: 15.3 MB (21.73%) - LeetHub

---
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.cpp
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        if(root==NULL) return {};
+        vector<int> ans;
+        stack<pair<Node*,bool>> st;
+        st.push({root,false});
+        while(!st.empty())
+        {
+            Node* node=st.top().first;
+            bool vis=st.top().second;
+            st.pop();
+            if(vis==true){
+                ans.push_back(node->val);
+            }
+            else{
+                st.push({node,true});
+                for(int i=(node->children.size())-1;i>=0;i--)
+                {
+                    st.push({node->children[i],false});
+                }
+            }
+        }
+        return ans;
+    }
+};
