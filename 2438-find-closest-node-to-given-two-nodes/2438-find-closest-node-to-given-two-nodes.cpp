7342e2d51f4690864b0b8fcbbe51f4be60f9efbd
Nirmali Behera <136896725+NirmaliB11@users.noreply.github.com>
2025-05-30 23:03:26 +0530

    Time: 16 ms (65%), Space: 101.8 MB (92.17%) - LeetHub

--- 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.cpp
+++ 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.cpp
@@ -1,27 +1,33 @@
+
 class Solution {
 public:
-    void dfs(int current, int distance, const vector<int>& edges, vector<int>& distances) {
-        while (current != -1 && distances[current] == -1) {
-            distances[current] = distance++;
-            current = edges[current];
-        }
-    }
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        vector<int> dist1 = getDistances(edges, node1);
+        vector<int> dist2 = getDistances(edges, node2);
+        int result = -1;
+        int minDistance = INT_MAX;
 
-    int closestMeetingNode(vector<int>& edges, int start1, int start2) {
-        int res = -1, Min_Of_Max = INT_MAX, n = edges.size();
-        vector<int> dist1(n, -1), dist2(n, -1);
-        dfs(start1, 0, edges, dist1);
-        dfs(start2, 0, edges, dist2);
-
-        for (int i = 0; i < n; i++) {
-            if (dist1[i] >= 0 && dist2[i] >= 0) {
+        for (int i = 0; i < edges.size(); i++) {
+            if (dist1[i] != -1 && dist2[i] != -1) {
                 int maxDist = max(dist1[i], dist2[i]);
-                if (maxDist < Min_Of_Max) {
-                    Min_Of_Max = maxDist;
-                    res = i;
+                if (maxDist < minDistance) {
+                    minDistance = maxDist;
+                    result = i;
                 }
             }
         }
-        return res;
+        return result;
+    }
+
+private:
+    vector<int> getDistances(const vector<int>& edges, int start) {
+        int n = edges.size();
+        vector<int> dist(n, -1);
+        int d = 0;
+        while (start != -1 && dist[start] == -1) {
+            dist[start] = d++;
+            start = edges[start];
+        }
+        return dist;
     }
 };
